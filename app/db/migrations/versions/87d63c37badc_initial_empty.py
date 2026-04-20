f1776c91fab58f048071eedbb1e74a21ef9febc0
yawnsduzin <yawnsduzin@gmail.com>
2026-04-20 09:47:42 +0900

    feat: initialize SQLAlchemy base, session, and Alembic migrations

---
+++ app/db/migrations/versions/87d63c37badc_initial_empty.py
@@ -0,0 +1,28 @@
+"""initial empty
+
+Revision ID: 87d63c37badc
+Revises: 
+Create Date: 2026-04-20 09:47:08.323370
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '87d63c37badc'
+down_revision: Union[str, Sequence[str], None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
